10af922e693ec8b8281c5aa9152296e4bba2f62a
Gabriel Oliveira <gabrieloliveira.gho@gmail.com>
2021-07-18 17:10:01 -0300

    feat: preview

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -1,7 +1,26 @@
 import { FC, useState } from "react";
 import Dropzone from "react-dropzone";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    width: 350,
+    height: 350,
+    backgroundColor: "rgba(121, 143, 111, 60%)",
+    border: "10px solid rgb(121, 143, 111)",
+  },
+  preview: {
+    maxHeight: "100%",
+    maxWidth: "100%",
+    objectFit: "contain",
+  },
+});
 
 export const ImageUpload: FC = () => {
+  const classes = useStyles();
   const [fileUrl, setFileUrl] = useState("");
   const updatePreview = ([file]: File[]) => {
     if (fileUrl) {
@@ -17,12 +36,18 @@ export const ImageUpload: FC = () => {
       multiple={false}
       noClick={false}
       onDropAccepted={updatePreview}
+      onDrop={(e) => console.log(e)}
     >
       {({ getRootProps, getInputProps }) => (
-        <div {...getRootProps()}>
+        <div {...getRootProps()} className={classes.root}>
+          {fileUrl && (
+            <img
+              className={classes.preview}
+              src={fileUrl}
+              alt="uploaded image preview"
+            />
+          )}
           <input {...getInputProps()} />
-          <p>Drag 'n' drop some files here, or click to select files</p>
-          {fileUrl && <img src={fileUrl} />}
         </div>
       )}
     </Dropzone>
