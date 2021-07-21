1038cc0e2437b1ade75a815974e3f207572b9ab7
Gabriel Oliveira <gabrieloliveira.gho@gmail.com>
2021-07-20 22:27:39 -0300

    feat: correction on useLoadItems hook

--- src/components/SameBreedGallery/useLoadItems.ts
+++ src/components/SameBreedGallery/useLoadItems.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useEffect, useState } from "react";
 
 type UseLoadItemsHook = {
   loading: boolean;
@@ -14,8 +14,11 @@ export const useLoadItems = (imageUrls: string[]): UseLoadItemsHook => {
   const [items, setItems] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
 
+  useEffect(() => {
+    setItems([]);
+  }, []);
+
   const hasNextPage = items.length < imageUrls.length;
-  console.log(items.length, imageUrls.length, hasNextPage);
 
   const loadMore = () => {
     if (!hasNextPage) {
@@ -27,12 +30,6 @@ export const useLoadItems = (imageUrls: string[]): UseLoadItemsHook => {
 
     const sliceStartIndex = page * ITEMS_PER_PAGE;
     let sliceEndIndex = sliceStartIndex + ITEMS_PER_PAGE;
-
-    console.log(sliceStartIndex, sliceEndIndex);
-
-    if (!imageUrls[sliceEndIndex]) {
-      sliceEndIndex = imageUrls.length;
-    }
 
     const itemsToAdd = [
       ...items,
